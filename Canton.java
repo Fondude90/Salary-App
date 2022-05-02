ad94025feeda2725fa662fd07a28224edb7eb23b
Tom <76867875+tomh90@users.noreply.github.com>
2022-05-02 22:53:02 +0200

    Create class Canton
    new class for Canton objects, plus methods etc

---
+++ Canton.java
@@ -0,0 +1,34 @@
+package com.tom.mainGui;
+
+public class Canton {
+    private String name;
+    private double taxRate;
+
+    public Canton(String initialName){
+        name = initialName;
+        taxRate = 0;
+    }
+
+    public double getTaxRate() {
+
+        return taxRate;
+    }
+
+    public String getName() {
+
+        return name;
+    }
+
+    public void updateCantonRate(double newRate){
+
+        taxRate = newRate;
+    }
+    public void updateCantonName(String newName){
+
+        name = newName;
+    }
+
+    public void printObject(){
+        System.out.println("Canton: " + name + ", tax rate: " + taxRate);
+    }
+}
